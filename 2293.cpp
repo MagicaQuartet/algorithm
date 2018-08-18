92887f0ab65025dc95609ec8062c031731282ec6
MagicaQuartet <dev.dskim@gmail.com>
2018-08-18 17:57:55 +0900

    Dynamic Programming (2018/8/18) - correct wrong code

--- 2293.cpp
+++ 2293.cpp
@@ -1,43 +1,52 @@
 // Backjoon Online Judge #2293
 // (Dynamic Programming)
 
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
 #include <vector>
 
 using namespace std;
 
 int main() {
-	int n, k;
-	cin >> n >> k;
+	int n, k, temp, result;
+	scanf("%d %d", &n, &k);
 
-	int *coin_arr = new int[n];
-	long long *cnt = new long long[k+1];
+	vector<int> coin;
+	vector<vector<int>> dp;
+
 	for (int i = 0; i < n; i++) {
-		int temp;
-		cin >> temp;
-		coin_arr[i] = temp;
+		scanf("%d", &temp);
+		coin.push_back(temp);
 	}
 
-	cnt[0] = 0;
-
-	vector<int> coin(coin_arr, coin_arr + n);
 	sort(coin.begin(), coin.end());
 
-	for (int i = 1; i < k+1; i++) {
-		cnt[i] = 0;
-		for (vector<int>::iterator it=coin.begin(); it!=coin.end(); ++it) {
-			if (*it <= i) {
-				if (cnt[i-*it] == 0)
-					cnt[i] = 1;
-				else
-					cnt[i] += cnt[i-*it];
-			}
+	for (int i = 0; i < k; i++) {
+		dp.push_back(vector<int>());
+		for (int j = 0; j < n; j++) {
+			if (i == coin[j]-1)
+				dp.back().push_back(1);
 			else
-				break;
+				dp.back().push_back(0);
 		}
 	}
 
-	cout << cnt[k] << endl;
+	for (int i = coin[0]-1; i < k; i++) {
+		for (int j = 0; j < n; j++) {
+			if (dp[i][j] != 0) {
+				for (int l = j; l < n; l++)	{
+					if (i + coin[l] < k) {
+						dp[i + coin[l]][l] += dp[i][j];
+					}
+				}
+			}
+		}
+	}
+
+	result = 0;
+	for (int i = 0; i < n; i++) {
+		result += dp[k-1][i];
+	}
+	printf("%d\n", result);
 	return 0;
 }
