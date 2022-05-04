5c1020235ad4f29272daa34b30126b12f316eb1d
MagicaQuartet <dev.dskim@gmail.com>
2022-05-04 13:41:49 +0900

    leetcode 55 풀이 수정

--- leetcode/55.cpp
+++ leetcode/55.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int dp[10010];
-        memset(dp, -1, sizeof(dp));
-        return dfs(nums, dp, 0) == 1;
-    }
-    
-    int dfs(const vector<int>& nums, int* dp, int idx) {
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
-        if (idx + nums[idx] >= nums.size()-1) {
-            return dp[idx] = 1;
-        }
-        if (nums[idx] == 0) {
-            return dp[idx] = 0;
-        }
-        for (int i=nums[idx]; i>0; i--) {
-            if (dfs(nums, dp, idx+i)) {
-                return dp[idx] = 1;
+        int idx = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (idx < i) {
+                break;
+            }
+            idx = max(idx, i + nums[i]);
+            if (idx + 1 >= nums.size()) {
+                return true;
             }
         }
-        return dp[idx] = 0;
+        return false;
     }
 };
 
 /* DP를 활용하면 각 인덱스에서의 도달 가능 여부를 일일이 찾아도 linear time에 가능하다 */
+/* 수정: 생각해보니 DP 같은 거 안 쓰고 순회하면서 도달 가능한 최대 인덱스 갱신하고 그 인덱스를 벗어나면 실패 처리 해주면 된다 */
